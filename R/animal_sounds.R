5fd425554c3d3c2791037bf557bbb30ba9869250
Stefany Coxe <istefany@gmail.com>
2021-02-17 11:35:39 -0500

    Initial commit

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,3 +1,12 @@
+#' Return animal's sound
+#'
+#' @param animal An animal
+#' @param sound A sound
+#' @return A statement about the animal's sound
+#' @examples
+#' animal_sounds("dog", "woof")
+#' animal_sounds("cat", "meow")
+
 animal_sounds <- function(animal, sound) {
    assertthat::assert_that(
      assertthat::is.string(animal),
